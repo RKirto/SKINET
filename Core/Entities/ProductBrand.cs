e6a3e57631ccb2d7a9431bcfd3dc610b9978f5ec
Rati Kirtadze <rkirto1@gmail.com>
2022-07-18 12:54:04 +0400

    dev

---
+++ Core/Entities/ProductBrand.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace Core.Entities
+{
+    public class ProductBrand : BaseEntity
+    {
+        public string Name { get; set; }
+    }
+}
+
---
+++ Core/Entities/ProductType.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace Core.Entities
+{
+    public class ProductType : BaseEntity
+    {
+        public string Name { get; set; }
+    }
+}
+

5e5e3c5cfa12e0ead9b892c52c350d1013392be8
Rati Kirtadze <rkirto1@gmail.com>
2022-07-29 16:30:33 +0400

    DEV

---
+++ API/Errors/ApiResponse.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Errors
+{
+    public class ApiResponse
+    {
+        public ApiResponse(int statusCode, string message = null)
+        {
+            StatusCode = statusCode;
+            Message = message ?? GetDefaultMessageForStatusCode(statusCode);
+        }
+
+        public int StatusCode { get; set; }
+        public string Message { get; set; }
+
+        private string GetDefaultMessageForStatusCode(int statusCode)
+        {
+            return statusCode switch
+            {
+                400 => " 400 ",
+                401 => " 401 ",
+                404 => " 404 ",
+                500 => " 500 ",
+                _ => null
+            };
+        }
+    }
+}
